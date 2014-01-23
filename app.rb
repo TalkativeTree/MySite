8c811708b9d645cd9d9b60711eb28e1fe1df2fe2
Ben Angel <benjamin.b2@gmail.com>
2014-01-22 16:38:32 -0800

    added get for stylesheets to app.rb

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ require './environments'
 require 'sinatra/flash'
 require 'sinatra/redirect_with_flash'
 
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+end
+
 get "/" do
   erb :"application"
 end
