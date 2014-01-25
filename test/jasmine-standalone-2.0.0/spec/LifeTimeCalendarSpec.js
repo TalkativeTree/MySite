b4120a41970c5ed44b52dc3e543ee084948dd20c
Ben Angel <benjamin.b2@gmail.com>
2014-01-24 19:09:57 -0800

    added spec for LifeTi meCalendar

---
+++ test/jasmine-standalone-2.0.0/spec/LifeTimeCalendarSpec.js
@@ -0,0 +1,10 @@
+describe("LifeTimeCalendar", function(){
+  beforeEach(function(){
+    ltc = new LifeTimeCalendar();
+    custom_ltc = new LifeTimeCalendar(400, 500);
+  });
+
+  it("should have default height and width", function(){
+    expect(ltc.DEFAULTS.heigher).toEqual()
+  });
+});
