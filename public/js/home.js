0a52eca09155ccc8863f5c360aa0ec96318ad5ee
Ben Angel <benjamin.b2@gmail.com>
2014-01-30 23:50:51 -0800

    removed line from mixins.scss

---
+++ public/js/home.js
@@ -0,0 +1,18 @@
+function fillArray(elements, filler){
+  array = [];
+  for(var i = 0; i < elements; i++){
+    array.push(filler);
+  }
+}
+
+function Life(start){
+  this.LENGTH = 78;
+  this.birth = start;
+  this.years = [];
+  this.today = new Date();
+}
+
+function Month(month, activities){
+  this.name = month;
+  this.activities = activities;
+}
